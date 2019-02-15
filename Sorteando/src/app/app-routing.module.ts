56190d6b4453be05441d6c9d7c223faf6fb4f297
FabioJS <fabiodossantospy@gmail.com>
2019-02-14 23:37:21 -0200

    Criada a página Sobre e modificada a página Home

--- Sorteando/src/app/app-routing.module.ts
+++ Sorteando/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
+  },
+  {
+    path: 'sobre',
+    loadChildren: './sobre/sobre.module#SobrePageModule'
   }
 ];
 
